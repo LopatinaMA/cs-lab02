e0c0b527efc2d13a284a02c22f1930df6a969620
Bob (LopatinaMA) <LopatinaMA@mpei.ru>
2022-03-21 15:17:45 +0300

    решение конфликта

--- main.cpp
+++ main.cpp
@@ -8,17 +8,11 @@ int main()
     int a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
-<<<<<<< HEAD
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n'
-         << "Min = " << min(a, b) << '\n';
-=======
-    << "A - B = " << a - b << '\n'
-    << "A * B = " << a * b << '\n'
-    << "A / B = " << a / b << '\n'
-    << "Max = " << max(a, b) << '\n';
+         << "Min = " << min(a, b) << '\n'
+         << "Max = " << max(a, b) << '\n';
 
->>>>>>> caa0408645d0b06e11370ec782e2bb898d1a081c
     return 0;
 }
